c8f179399dbeb9e94e580273763b9bdc1cc4d281
Hyeonah Jung <gwi02378@gmail.com>
2021-04-12 22:47:27 +0900

    DailyTemperature - Stack 문제

--- DailyTemperature.kt
+++ DailyTemperature.kt
@@ -3,10 +3,27 @@ import java.util.*
 // 하루 온도의 리스트가 있을 때, 각각의 날들에서 더 따뜻한 날이 되려면
 // 며칠을 기다려야 하는지를 담은 리스트를 반환하여라 (미래에 따뜻한 날이 없을 시 0 반환)
 fun main() {
+    val nums = intArrayOf(73, 74, 75, 71, 69, 72, 76, 73)
+    val res = dailyTemperatures(nums)
 
+    for (i in res) {
+        print("$i ")
+    }
 }
 
 fun dailyTemperatures(temper: IntArray): IntArray {
     val stack = Stack<Int>()
-    return temper
+    val result = IntArray(temper.size)
+
+    for (i in temper.indices) {
+        println("temper : ${temper[i]}")
+        while (!stack.isEmpty() && temper[stack.peek()] < temper[i]) {
+            val index = stack.pop()
+            println("i : $i, index : $index")
+            result[index] = i-index
+        }
+        stack.push(i)
+    }
+
+    return result
 }
