9541d1e73eeabecbb86de22d014ab8af8756dda1
JHyeonah <38305511+JHyeonah@users.noreply.github.com>
2022-02-17 22:27:17 +0900

    Create CompareNumberAsString.java
    가장 큰 수

---
+++ CompareNumberAsString.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class CompareNumberAsString {
+    public String solution(int[] numbers) {
+        String answer = "";
+        String[] strNumbers = new String[numbers.length];
+        
+        for (int i = 0; i < numbers.length; i++) {
+            strNumbers[i] = Integer.toString(numbers[i]);
+        }
+        
+        Arrays.sort(strNumbers, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return Integer.parseInt(o2 + o1) - Integer.parseInt(o1 + o2);
+            }
+        });
+        
+        for (int i = 0; i < numbers.length; i++) {
+            answer = answer.concat(strNumbers[i]);
+        }
+        
+        return answer;
+    }
+}
